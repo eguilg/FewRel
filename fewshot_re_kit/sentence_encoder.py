70c318c19d72b59c4bf7d4586ce755858e29bb9c
cqEguil <artisan6g@outlook.com>
2020-03-26 17:28:02 +0800

    set cnn encoder posemb dim back to 5

--- fewshot_re_kit/sentence_encoder.py
+++ fewshot_re_kit/sentence_encoder.py
@@ -11,7 +11,7 @@ from transformers import BertTokenizer, BertModel, BertForMaskedLM, BertForSeque
 class CNNSentenceEncoder(nn.Module):
 
     def __init__(self, word_vec_mat, word2id, max_length, word_embedding_dim=50, 
-            pos_embedding_dim=50, hidden_size=230):
+            pos_embedding_dim=5, hidden_size=230):
         nn.Module.__init__(self)
         self.hidden_size = hidden_size
         self.max_length = max_length
